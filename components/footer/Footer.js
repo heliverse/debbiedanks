a27b7958203faa57e9bbe800dab7dd9cce972363
prakashjyoti123 <prakashjyotirai97@gmail.com>
2022-07-15 03:56:43 +0530

    Home Page CSS Improved by jyotit

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./Footer.module.css";
-import { H1 } from "../H1.styled";
 
-import { Button } from "../Button.styled";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -13,18 +11,18 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.first}>
         <div className={styles.heading}>
-          <H1>
+          <h2>
             Join 2M+ professionals getting the <br /> Hustle daily news brief
-          </H1>
+          </h2>
         </div>
         <div className={styles.inputWithButton}>
           <input type="text" placeholder="Your email address" />
-          <Button>Join Free</Button>
+          <button>Join Free</button>
         </div>
       </div>
       <div className={styles.second}>
         <div className={styles.secondHeading}>
-          <H1>M</H1>
+          <h2>M</h2>
         </div>
         <div className={styles.secondContent}>
           <div className={styles.spanText}>
--- components/homePage/HomePage.js
+++ components/homePage/HomePage.js
@@ -1,24 +1,22 @@
 import React from "react";
 import Image from "next/image";
-import { H1 } from "../repeating/TextFormatting.jsx/index.js";
-import { H3 } from "../H3.styled";
+// import { H1 } from "../repeating/TextFormatting.jsx/index.js";
+// import { H3 } from "../H3.styled";
 import styles from "./HomePage.module.css";
-import { Button } from "../repeating/TextFormatting.jsx/index.js";
+// import { Button } from "../repeating/TextFormatting.jsx/index.js";
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
-      <div>
-        <H1>
-          Trending news delivered to <br /> your inbox daily.
-        </H1>
-        <H3>
+      <div className={styles.left}>
+        <h2>Trending news delivered to your inbox daily.</h2>
+        <h3>
           Get the <span>5-minute newsletter</span> keeping 2M+ innovators in the
           loop.
-        </H3>
+        </h3>
         <div className={styles.inputWithButton}>
           <input type="text" placeholder="Your email address" />
-          <Button>Subscribe</Button>
+          <button>Subscribe</button>
         </div>
       </div>
       <div className={styles.femaleBear}>
